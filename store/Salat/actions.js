f95b0603daa1e3ae6ec11c3293548760d7b307a7
Muhar Dwi Purwanto <k@muhar.us>
2019-11-20 20:55:41 +0700

    ADD: response error check

--- store/Salat/actions.js
+++ store/Salat/actions.js
@@ -2,19 +2,24 @@ export default {
   getPrayTimes ({ commit }) {
     return new Promise ((resolve, reject) => {
       this.$axios.$get('https://api.lab.muhar.us/salat/').then((response) => {
-        const r = response[0]
-        const times = {
-          subuh: r.fajr,
-          zuhur: r.dhuhr,
-          asar: r.asr,
-          magrib: r.maghrib,
-          isya: r.isha
+        if (!response.error) {
+          const r = response[0]
+          const times = {
+            subuh: r.fajr,
+            zuhur: r.dhuhr,
+            asar: r.asr,
+            magrib: r.maghrib,
+            isya: r.isha
+          }
+
+          commit('SET_TIMES', times)
+          commit('SET_DATE', r.date_for)
+
+          resolve(response)
         }
-
-        commit('SET_TIMES', times)
-        commit('SET_DATE', r.date_for)
-
-        resolve(response)
+        else {
+          reject(response)
+        }
       }).catch((error) => {
         reject(error)
       })
